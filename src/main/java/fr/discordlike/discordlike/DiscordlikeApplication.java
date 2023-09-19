b9cafb6dadb9af44b2c4c824220d428e5db07e0b
Boulay nathan <nathan.boulay@orange.fr>
2023-09-19 13:53:12 +0200

    initialisation projet

---
+++ src/main/java/fr/discordlike/discordlike/DiscordlikeApplication.java
@@ -0,0 +1,13 @@
+package fr.discordlike.discordlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordlikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordlikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/discordlike/discordlike/DiscordlikeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.discordlike.discordlike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordlikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
